3bc1351a8b8baaa67b69e4c76f61612a5d5ab48e
im <poson_hemp@yahoo.co.jp>
2023-08-03 08:26:32 +0900

    add test/ test_is_human_message, test_is_ai_message

--- src/tests/test_langchain_chat.py
+++ src/tests/test_langchain_chat.py
@@ -3,12 +3,28 @@ from src.function.langchain_chat import LangchainChat
 
 
 @pytest.fixture
-def langchain_chat():
+def langchain_chat() -> LangchainChat:
     return LangchainChat()
 
 
-def test_human_message_content(langchain_chat):
+def test_human_message_content(langchain_chat: LangchainChat) -> None:
     question = "Hello, I am human. How do you do?"
     human_message = langchain_chat.human_message_content(question)
 
     assert human_message.content == question
+
+
+def test_is_human_message(langchain_chat: LangchainChat) -> None:
+    message = "Hello, I am human. How do you do?"
+    human_message = langchain_chat.human_message_content(message)
+
+    assert langchain_chat.is_human_message(human_message) is True
+    assert langchain_chat.is_ai_message(human_message) is False
+
+
+def test_is_ai_message(langchain_chat: LangchainChat) -> None:
+    response = "I'm AI."
+    ai_message = langchain_chat.ai_message(response)
+
+    assert langchain_chat.is_ai_message(ai_message) is True
+    assert langchain_chat.is_human_message(ai_message) is False
